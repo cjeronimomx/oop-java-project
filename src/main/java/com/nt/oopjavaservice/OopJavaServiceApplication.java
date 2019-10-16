f992150bf764e789c9c850e26e6c0bfa68150222
Crisanto Jeronimo Garcia <crisanto.jeronimo.next@bbva.com>
2019-10-16 11:26:21 -0500

    Initial commit

---
+++ src/main/java/com/nt/oopjavaservice/OopJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nt.oopjavaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopJavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopJavaServiceApplication.class, args);
+	}
+
+}
