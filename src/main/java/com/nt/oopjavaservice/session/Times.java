a71d426628c7ea84b0ce774f9c4072fca828b5cc
kharenrm <karen.rosasmls@gmail.com>
2019-11-06 12:51:16 -0600

    Ejemplos de date, datetime y time

--- src/main/java/com/nt/oopjavaservice/session/Times.java
+++ src/main/java/com/nt/oopjavaservice/session/Times.java
@@ -1,10 +1,15 @@
 package com.nt.oopjavaservice.session;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.Month;
 
 public class Times {
     public static void main(String[] args) {
-        times();
+        //times();
+        dates();
+        datetimes();
     }
 
     public static void times(){
@@ -16,6 +21,23 @@ public class Times {
     }
 
     public static void dates(){
+        LocalDate localDate = LocalDate.now();
+        localDate = localDate.minusWeeks(2);
+        System.out.println("Fecha actual " + localDate);
 
+        LocalDate futureDate = LocalDate.of(2020, Month.AUGUST, 14);
+        futureDate.plusYears(1);
+        System.out.println("Cumpleaños " + futureDate);
+    }
+
+    public static void datetimes(){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println("Fecha actual " + localDateTime);
+
+        LocalTime localTime = localDateTime.toLocalTime();
+        System.out.println("Hora datetime " + localTime);
+
+        LocalDate localDate = localDateTime.toLocalDate();
+        System.out.println("Fecha datetime " + localDate);
     }
 }
